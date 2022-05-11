07fd7e89bb515f9898ad1c9d79743e6b0dd762fb
‘AMAYA <aamaya547@gmail.com>
2022-05-11 12:52:58 -0700

    completed getDogs and getDogs function in fetchUtils, now displaying dogs on main page

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
+
 // on load
+async function loadData() {
+    //declaring variable to database
+    //give file access to database and declaring it to a variable
+    const dogs = await getDogs();
+    // console.log(dogs);
+
+    for (let dog of dogs) {
+        const dogDiv = renderDogCard(dog);
+        dogListContainer.append(dogDiv);
+    }
+}
+loadData();
 // fetch all dogs
 // render and append all dog cards to the container
--- detail/detail.js
+++ detail/detail.js
@@ -3,6 +3,12 @@ import { renderDogDetail } from '../render-utils.js';
 
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
+
+// similar stuff pull up display list from scratch 
+// think about what happened in the details page
+// get dog function rather than findById
+
+
 // on load
 // get the id from URL
 // use the id to fetch the dog
